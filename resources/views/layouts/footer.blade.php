5a7a91a36e3d677f1ba86fa0d65be30901118f5a
Manh Tuan <fakernguyen123@gamil.com>
2025-01-16 22:18:39 +0700

    Refactor code

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,20 +1,3 @@
-{{-- <footer>
-    <div class="d-flex flex-md-row mx-0 justify-content-md-between px-md-4 flex-column-reverse align-items-center" style="font-size: .9em;">
-        <span class="m-0 copyright">&copy; {{ date('Y') }} Nhatrototsaigon. Mọi Quyền Đều Được Bảo Lưu.</span>
-        <ul class="mb-0 d-md-flex justify-content-md-end mx-0 px-md-1 px-4 w-100 d-flex flex-row" style="gap: 5px 15px; list-style-type: none;">
-            <li>
-                <a class="text-white" href="{{ route('privacy.index') }}">
-                    Chính sách bảo mật
-                </a>
-            </li>
-            <li>
-                <a class="text-white" href="/sitemap.xml">
-                    Sơ đồ trang web
-                </a>
-            </li>
-        </ul>
-    </div>
-</footer> --}}
 <footer class="bg-dark text-light py-5 mt-5">
     <div class="container">
         <div class="row text-start">
