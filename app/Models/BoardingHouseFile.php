c76ffd96377fbb7a711be5bdc1af155baae8485f
Manh Tuan <fakernguyen123@gamil.com>
2025-07-31 18:52:38 +0700

    Fix exception

--- app/Models/BoardingHouseFile.php
+++ app/Models/BoardingHouseFile.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class BoardingHouseFile extends Model
 {
     use HasFactory;
+
+    public function boardingHouse()
+    {
+        return $this->belongsTo(BoardingHouse::class, 'boarding_house_id');
+    }
 }
