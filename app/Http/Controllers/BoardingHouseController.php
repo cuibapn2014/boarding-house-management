2d01baa20b3b70197d2a510726862b13fb8bb70e
Manh tuan <fakernguyen123@gmail.com>
2025-11-24 22:10:29 +0700

    Fix

--- app/Http/Controllers/BoardingHouseController.php
+++ app/Http/Controllers/BoardingHouseController.php
@@ -212,7 +212,7 @@ class BoardingHouseController extends Controller
             return $this->responseError('Dữ liệu không tồn tại hoặc đã bị xoá!');
         }
 
-        if ($boardingHouse->canEdit()) {
+        if (! $boardingHouse->canEdit()) {
             return $this->responseError('Không có quyền chỉnh sửa');
         }
 
@@ -257,7 +257,7 @@ class BoardingHouseController extends Controller
             return $this->responseError('Dữ liệu không tồn tại hoặc đã bị xoá!');
         }
 
-        if ($boardingHouse->canDelete()) {
+        if (! $boardingHouse->canDelete()) {
             return $this->responseError('Không có quyền xoá!');
         }
 
